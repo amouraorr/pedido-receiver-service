aab1c1f8df42e5e599e02044bbf9b4ba54ce3945
amour <amouraorr@yahoo.com.br>
2025-07-16 08:06:13 -0300

    Ajuste de pacotes

--- src/main/java/com/fiap/receiver/message/KafkaProducerConfig.java
+++ src/main/java/com/fiap/receiver/message/KafkaProducerConfig.java
@@ -1,6 +1,6 @@
 package com.fiap.receiver.message;
 
-import com.fiap.receiver.domain.PedidoReceiver;
+import com.fiap.receiver.dto.request.PedidoReceiverRequestDTO;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -13,22 +13,20 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-// Configuração do Kafka Producer para serializar objetos Pedido como JSON
 @Configuration
 public class KafkaProducerConfig {
 
     @Bean
-    public ProducerFactory<String, PedidoReceiver> producerFactory() {
+    public ProducerFactory<String, PedidoReceiverRequestDTO> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "${spring.kafka.bootstrap-servers}");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        // Ajustar as propriedades do producer conforme necessário
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, PedidoReceiver> kafkaTemplate() {
+    public KafkaTemplate<String, PedidoReceiverRequestDTO> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }

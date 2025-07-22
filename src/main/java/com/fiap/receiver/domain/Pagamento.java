b73dc122879d820b399622a036728cdb0ec1fbc7
amour <amouraorr@yahoo.com.br>
2025-07-22 08:28:33 -0300

    Ajuste nesessários.

--- src/main/java/com/fiap/receiver/domain/Pagamento.java
+++ src/main/java/com/fiap/receiver/domain/Pagamento.java
@@ -1,8 +1,14 @@
 package com.fiap.receiver.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pagamento {
 
     private String numeroCartao;
--- src/main/java/com/fiap/receiver/domain/PedidoReceiver.java
+++ src/main/java/com/fiap/receiver/domain/PedidoReceiver.java
@@ -1,9 +1,16 @@
 package com.fiap.receiver.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PedidoReceiver {
     private String clienteId;
     private List<Item> itens;
